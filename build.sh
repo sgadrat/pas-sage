5ee29b8b2b77bb6dd3b6c1b201dcd498ee83cd34
Sylvain Gadrat <sgadrat@wontfix.it>
2019-05-18 10:00:33 +0200

    integration begin

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 set -e
+set -x
 
-tools/generate_bg_data.py tilemap assets/dummy_bg.png > game/game_states/bg_data.asm
-tools/generate_bg_data.py tiles assets/dummy_bg.png > game/chr_rom_bg.asm
-tools/generate_bg_data.py palettes assets/dummy_bg.png > game/game_states/bg_palettes.asm
+tools/generate_bg_data.py tilemap assets/dummy_bg.gif > game/game_states/bg_data.asm
+tools/generate_bg_data.py tiles assets/dummy_bg.gif > game/chr_rom_bg.asm
+tools/generate_bg_data.py palettes assets/dummy_bg.gif > game/game_states/bg_palettes.asm
 #xa nine.asm -C -o game\(E\).nes
 ~/workspace/xa-listing/xa/xa nine.asm -C -o game\(E\).nes -P - | python2 ~/workspace/xa-listing/listing2nl.py > game\(E\).nes.0.nl
